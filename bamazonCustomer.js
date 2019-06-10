a44d09275a5557005884c163c4a260aab01ac4f9
rasenin <romansenin15@gmail.com>
2019-06-10 12:28:02 -0700

    connected to database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,18 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+
+let connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+
+let args = process.argv;
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
+
