145121787c82ca812695f98d485c23c01c21dab6
rasenin <romansenin15@gmail.com>
2019-06-10 17:36:09 -0700

    finished app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,21 +11,100 @@ let connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
+  console.log(" \n--- Welcome to BAmazon! ---\n ");
+  next();
 });
 
-connection.query("SELECT * FROM products ORDER BY department_name", function(err, rows) {
-  if (err) throw err;
-  // console.log(rows);
-  displayResults(rows);
-});
+function next() {
+  inquirer
+    .prompt({
+      name: "choice",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["Buy an item", "Quit"]
+    })
+    .then(function(answer) {
+      if (answer.choice === "Quit") {
+        connection.end();
+      } else {
+        buyItem();
+      }
+    });
+}
 
-function displayResults(results) {
-  for(let i = 0; i < results.length; i++) {
-    console.log("-".repeat(10));
-    for (field in results[i]) {
-      console.log(field + ": " + results[i][field]);
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, rows) {
+    for (let i = 0; i < rows.length; i++) {
+      console.log("\n" + "-".repeat(10) + "\n");
+      for (field in rows[i]) {
+        console.log(field + ": " + rows[i][field]);
+      }
     }
-  }
-  console.log("-".repeat(10));
+    console.log("\n" + "-".repeat(10) + "\n");
+  });
+}
+
+function buyItem() {
+  displayProducts();
+  connection.query("SELECT COUNT(*) AS count FROM PRODUCTS", function(
+    err,
+    rows
+  ) {
+    if (err) throw err;
+    let itemCount = rows[0].count;
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "Enter the id of the item you wish to buy:",
+          validate: function validateId(id) {
+            return id <= itemCount && id >= 1;
+          }
+        }
+      ])
+      .then(function(idAns) {
+        inquirer
+          .prompt([
+            {
+              name: "amount",
+              type: "input",
+              message:
+                "Enter the amount of the product you'd like to purchase:",
+              validate: function validateAmount(amount) {
+                return parseInt(amount) > -1;
+              }
+            }
+          ])
+          .then(function(amountAns) {
+            connection.query(
+              "SELECT price, stock_quantity FROM products WHERE item_id = ?",
+              [idAns.id],
+              function(err, rows) {
+                if (err) throw err;
+                if (rows[0].stock_quantity < parseInt(amountAns.amount)) {
+                  console.log("Insufficient quantity!");
+                  next();
+                } else {
+                  connection.query(
+                    "UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+                    [
+                      rows[0].stock_quantity - parseInt(amountAns.amount),
+                      idAns.id
+                    ],
+                    function(err) {
+                      if (err) throw err;
+                      console.log(
+                        "The total cost of your purchase is $" +
+                          rows[0].price * parseInt(amountAns.amount)
+                      );
+                      next();
+                    }
+                  );
+                }
+              }
+            );
+          });
+      });
+  });
 }
